722c5d007f311ad26689930df1f1b3ca2a94d5d2
begobbi <beatriz.gobbi@uclouvain.be>
2019-01-22 16:47:50 +0100

    soil cover

--- working_grid_level.R
+++ working_grid_level.R
@@ -123,7 +123,6 @@ canopy<-rc>5
 v <- extract(canopy, sub_gridspdf)
 canopy_cover<-sapply(v, function(x) if (!is.null(x)) { sum(x)/length(x)} else NA)
 
-
 ##density
 fun1=function(x){x>5}
 poly<-rasterToPolygons(rc, fun1,dissolve=TRUE)
@@ -145,11 +144,79 @@ density<-cdata[3]/sub_gridspdf@data$area
 
 CHM_in_patches <- extract(CHM, p)
 max_perunit <-sapply(CHM_in_patches, function(x) if (!is.null(x)) { max(x)} else NA)
-
 max_spdf <- spCbind(patches_with_location, max_perunit)
-  
-
 mean_max <- ddply(max_spdf@data,"id", summarise,max = max(max_perunit))
 
+# soil cover
+soil<-rc<2
+soil_list <- extract(soil, sub_gridspdf)
+soil_cover<-sapply(soil_list, function(x) if (!is.null(x)) { sum(x)/length(x)} else NA)
 
+#raster to polygon
+fun2=function(x){x<2}
+poly2<-rasterToPolygons(rc, fun2, dissolve=TRUE)
+#explode
+SOIL<-ms_explode(poly2)
+SOIL$Area_sqm <- area(SOIL)
+#head(SOIL)
+newdata <- SOIL[which(SOIL@data$Area_sqm >10),]
+
+#soil cover
+soil_cover<-sum(newdata$Area_sqm)
+
+ground_density<-nrow(newdata@data)
+
+
+
+####using point cloud  ####
+
+
+
+
+####canopy_cover<-(cellStats(canopy,stat="sum")/length(CHM))*100
+CHM_in_grid <- extract(CHM, sub_gridspdf)
+canopy_cover<-sapply(v, function(x) if (!is.null(x)) { sum(x)/length(x)} else NA)
+pioneers_density<-length(pt_CHM@data[pt_CHM@data$V3>4 & pt_CHM@data$V3 < 6,"V3"])/length(pt_CHM@data[pt_CHM@data$V3 > 0,"V3"])*100
+
+
+
+#encroachment
+shrub_density<-length(pt_CHM@data[pt_CHM@data$V3>0.5 & pt_CHM@data$V3 < 4,"V3"])/length(pt_CHM@data[pt_CHM@data$V3 > 0,"V3"])*100
+
+
+
+
+########Shannon diversity 
+#classes
+suite1<-c(0:14)
+suite2<-c(1:15)
+suite3<-c(1:15)
+mat<-cbind(suite1,suite2,suite3)
+mat2<-cbind(mat,NA)
+mat2<-cbind(mat2,NA)
+bins <- reclassify(CHM, mat)
+v<-1
+for(v in suite3){
+  bin<-bins==v
+  p<-(cellStats(bin,stat="sum")/length(CHM))
+  p2<-p*log(p)
+  mat2[v,4]<-p
+  mat2[v,5]<-p2
+}
+
+
+cinco<-mat2[,5]
+vertical_complexity<--sum(cinco,na.rm = TRUE)
+
+quantiles<-quantile(pt_CHM@data$V3, c(.01,.05,0.1,.30,0.5, .60,.90, .99))
+
+last_percentile<-quantiles[[8]]
+
+difference_height<-quantiles[[7]]-quantiles[[3]]
+
+standard_deviation<-sd(pt_CHM@data$V3, na.rm = FALSE)
+
+
+
+vertical_distribution<-(last_percentile-median(pt_CHM@data$V3))/last_percentile
 
